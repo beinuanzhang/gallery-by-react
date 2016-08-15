4434369f8ec689f00fc041c5923346f81db7483d
beinuanzhang <beinanzhang@gmail.com>
2016-08-15 14:56:08 +0800

    aaaaa

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,49 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
 
-class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
-  }
-}
+var imageDatas = require('../data/imageDatas.json');
 
-AppComponent.defaultProps = {
-};
+imageDatas = (function genImageURL(imageDatasArr){
+	for (var i=0, j=imageDatasArr.length; i<j; i++) {
+		var singleImageDate = imageDatasArr[i];
 
-export default AppComponent;
+		singleImageDate.imageURL = require('../images'+singleImageDate.fileName);
+
+		imageDatasArr[i] = singleImageDate;
+	}
+	return imageDatasArr;
+})(imageDatas);
+
+var App = React.createClass({
+	render: function(){
+		return (
+			<section className="stage">
+				<section className="image-sec">
+				</section>
+				<nav className="controller-nav">
+				</nav>
+			</section>
+		);
+	}
+});
+
+
+// class AppComponent extends React.Component {
+//   render() {
+//     return (
+//       <div className="index">
+//         <img src={yeomanImage} alt="Yeoman Generator" />
+//         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+//       </div>
+//     );
+//   }
+// }
+
+// AppComponent.defaultProps = {
+// };
+
+// export default AppComponent;
